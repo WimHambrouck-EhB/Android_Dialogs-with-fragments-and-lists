63486add5300587755002ed518bf770296864998
Wim Hambrouck <wim.hambrouck@ehb.be>
2021-11-25 09:34:28 +0100

    Initial commit

---
+++ app/src/main/java/org/example/dialogswithfragmentsandlists/MainActivity.kt
@@ -0,0 +1,27 @@
+package org.example.dialogswithfragmentsandlists
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import org.example.dialogswithfragmentsandlists.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
